09ed827ce33396f14bd82cbf0e26951d2fbe6cce
hyomin <johyomin1119@gmail.com>
2022-06-29 18:04:38 +0900

    swiper-button 필요

--- js/script.js
+++ js/script.js
@@ -1,34 +1,27 @@
-$(document).ready(function(){
+$(document).ready(function () {
   // 팝업닫기
   let pop = $('.pop');
   let pop_close = $('.pop-close');
-  pop_close.click(function(event){
+  pop_close.click(function (event) {
     // a 태그의 href 로 이동하는 기능 막기
     event.preventDefault();
-    
     pop.hide();
   });
 });
 
-window.onload = function(){
+window.onload = function () {  
+  // swiper
+  new Swiper('.event-left', {
+    speed: 1000,
+    navigation: {
+      prevEl: '.event-left-prev',
+      nextEl: '.event-left-next'
+    },
+
+    pagination: {
+      el: '.event-left-pg',
+      type: 'fraction'
+    }
+  });
+
 };
-
-// swiper
-new Swiper('.event-left', {
-  loop: true,
-  speed: 1000,
-  autoplay: {
-    delay: 1000,
-    disableOnInteraction: false,
-  },
-  navigation: {
-    prevEl: '.sw-visual-prev',
-    nextEl: '.sw-visual-next'
-  },
-
-  pagination: {
-    el: '.sw-visual-pg',
-    type: 'fraction'
-  }
-});
-

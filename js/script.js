410e7a21d0fe0ddf2917e6a5909badcf4b24647d
hyomin <johyomin1119@gmail.com>
2022-07-16 00:53:20 +0900

    jeongyookgak clone coding complete

--- js/script.js
+++ js/script.js
@@ -1,4 +1,32 @@
 $(document).ready(function () {
+  // 안내창 기능
+  // 추가기능 : 스크롤바 없애기
+  $('html').css('overflow', 'hidden');
+
+  let modalWrap = $('.modal-wrap');
+  let modalClose = $('.modal-close');
+  modalClose.click(function () {
+    modalWrap.stop().fadeOut(100);
+    // 추가기능 : 스크롤바 살리기
+    $('html').css('overflow', 'auto');
+  });
+
+  let modalMain = $('.modal-main');
+  // 내용 배경 클릭
+  modalMain.click(function (event) {
+    // 클릭 정보 전달 막기
+    event.stopPropagation();
+  });
+  // 전체 배경 클릭
+  modalWrap.click(function () {
+    modalWrap.stop().fadeOut(100);
+    // 추가기능 : 스크롤바 살리기
+    $('html').css('overflow', 'auto');
+  });
+
+});
+
+$(document).ready(function () {
   // 팝업닫기
   let pop = $('.pop');
   let pop_close = $('.pop-close');
@@ -9,7 +37,7 @@ $(document).ready(function () {
   });
 });
 
-window.onload = function () {  
+window.onload = function () {
   // swiper
   new Swiper('.event-left', {
     speed: 1000,
